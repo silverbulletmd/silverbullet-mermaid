33f155a613660f3e2d6e222a7c0ea5d9645af014
Zef Hemel <zef@zef.me>
2022-12-22 16:59:50 +0100

    Don't break things when putting <'s in mermaid diagrams

--- mermaid.ts
+++ mermaid.ts
@@ -2,7 +2,7 @@ export function widget(
   bodyText: string,
 ): { html: string; script: string } {
   return {
-    html: `<pre class="mermaid">${bodyText}</pre>`,
+    html: `<pre class="mermaid">${bodyText.replaceAll("<", "&lt;")}</pre>`,
     script: `
     loadJsByUrl("https://cdn.jsdelivr.net/npm/mermaid@9/dist/mermaid.min.js").then(() => {
       mermaid.init().then(updateHeight);
