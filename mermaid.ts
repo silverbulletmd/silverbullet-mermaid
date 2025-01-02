a94eaa135e7ad9ef07206f1946fbaddbcebbe864
Joe Lombrozo <joe@djeebus.net>
2025-01-02 11:21:47 -0500

    upgrade deno, imports, mermaid, add configuration

--- mermaid.ts
+++ mermaid.ts
@@ -1,10 +1,16 @@
-export function widget(
+import { system } from "@silverbulletmd/silverbullet/syscalls";
+
+export async function widget(
   bodyText: string,
-): { html: string; script: string } {
+): Promise<CodeWidgetContent> {
+  const config = await system.getSpaceConfig("mermaid")
+  const mermaidVersion = config.version || "11.4.1"
+  const mermaidHash = config.integrity || "sha256-pDvBr9RG+cTMZqxd1F0C6NZeJvxTROwO94f4jW3bb54="
+
   return {
     html: `<pre class="mermaid">${bodyText.replaceAll("<", "&lt;")}</pre>`,
     script: `
-    loadJsByUrl("https://cdn.jsdelivr.net/npm/mermaid@10.9.3/dist/mermaid.min.js","sha256-Wo7JGCC9Va/vBJBoSJNpkQ5dbOcMgQOVLyfinT526Lw=").then(() => {
+    loadJsByUrl("https://cdn.jsdelivr.net/npm/mermaid@${mermaidVersion}/dist/mermaid.min.js", "${mermaidHash}").then(() => {
       mermaid.init().then(updateHeight);
     });
     document.addEventListener("click", () => {
