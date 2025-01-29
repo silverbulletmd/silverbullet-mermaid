0ad8b58d5f2ed0cc4f5da0364051fd6ff10ee5fe
Henrik Saksela <henrik.saksela@gmail.com>
2025-01-29 23:21:45 +0200

    Fix broken integrity config handling by adding missing quotes

--- mermaid.ts
+++ mermaid.ts
@@ -5,7 +5,7 @@ export async function widget(
 ): Promise<CodeWidgetContent> {
   const config = await system.getSpaceConfig("mermaid")
   const mermaidVersion = config.version || "11.4.1"
-  let mermaidHash : string | undefined = config.integrity ? "${config.integrity}" : "sha256-pDvBr9RG+cTMZqxd1F0C6NZeJvxTROwO94f4jW3bb54="
+  let mermaidHash : string | undefined = config.integrity ? `"${config.integrity}"` : `"sha256-pDvBr9RG+cTMZqxd1F0C6NZeJvxTROwO94f4jW3bb54="`
   if (config.integrity_disabled) {
     mermaidHash = undefined;
   }
